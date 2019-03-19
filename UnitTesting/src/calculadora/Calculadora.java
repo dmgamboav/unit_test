adaf3ac85594a24d7bf07ebe9eb9a67d049c14a1
Mauricio Gamboa Vasquez <mauricio.gamboa@avantica.net>
2019-03-18 17:36:48 -0700

    Added validation for unit testing

--- UnitTesting/src/calculadora/Calculadora.java
+++ UnitTesting/src/calculadora/Calculadora.java
@@ -6,5 +6,9 @@ public class Calculadora {
 		return num1 * num2;
 	}
 	
+	public double getSuma(Double num1, Double num2) {
+		return num1 + num2;
+	}
+	
 	
 }
--- UnitTesting/src/calculadoratest/CalculadoraTest.java
+++ UnitTesting/src/calculadoratest/CalculadoraTest.java
@@ -9,21 +9,28 @@ import org.junit.jupiter.api.Test;
 import calculadora.Calculadora;
 
 class CalculadoraTest {
-   private Calculadora calculadora;
+	private Calculadora calculadora;
+
 	@BeforeEach
 	void setUp() throws Exception {
 		calculadora = new Calculadora();
 	}
-	
 
 	@AfterEach
 	void tearDown() throws Exception {
 	}
 
 	@Test
-	void testMultiplicar() { 
-	    double resultado = calculadora.getMultiplicacion(10, 10);
+	void testMultiplicar() {
+		double resultado = calculadora.getMultiplicacion(10, 10);
 		assertEquals(100, resultado);
+	}
+
+	@Test
+	void testNullPointerException() {
+		assertThrows(NullPointerException.class, () -> {
+			calculadora.getSuma(null, 200d);
+		});
 	}
 
 }
