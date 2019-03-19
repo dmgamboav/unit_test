c27b5ee2c603766df28938cf64a699cd561b7983
Mauricio Gamboa Vasquez <mauricio.gamboa@avantica.net>
2019-03-18 17:24:11 -0700

    Unit test for a simple calculator.

---
+++ UnitTesting/src/calculadora/Calculadora.java
@@ -0,0 +1,10 @@
+package calculadora;
+
+public class Calculadora {
+
+	public double getMultiplicacion(double num1, double num2) {
+		return num1 * num2;
+	}
+	
+	
+}
---
+++ UnitTesting/src/calculadoratest/CalculadoraTest.java
@@ -0,0 +1,29 @@
+package calculadoratest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import calculadora.Calculadora;
+
+class CalculadoraTest {
+   private Calculadora calculadora;
+	@BeforeEach
+	void setUp() throws Exception {
+		calculadora = new Calculadora();
+	}
+	
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void testMultiplicar() { 
+	    double resultado = calculadora.getMultiplicacion(10, 10);
+		assertEquals(100, resultado);
+	}
+
+}
